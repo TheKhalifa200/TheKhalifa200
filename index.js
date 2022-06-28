b2d057705fde5104832b8bc0e96856f66800f26c
TheKhalifa200 <lovely_khalifa97@hotmail.com>
2022-06-28 10:39:00 +0000

    make it better

--- index.js
+++ index.js
@@ -18,10 +18,15 @@ const dialogflowFulfillment = (request, response) => {
     const agent = new WebhookClient({request, response})
 
     function membershipno(agent){
-        agent.add('www.google.com', function (error, response, body) {
+        agent.add('http://157.175.215.187:3090/Membership/Profile?membershipNo=26233', function (error, response, body) {
             if (!error && response.statusCode == 200) {
                  console.log(body)// Print the google web page.
-}})
+                 agent.add("/nIt did work!")
+}else{
+    agent.add("It did not work")
+}
+
+})
         
     }
 
