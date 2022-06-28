9334024f3a8af7608ca2c15aee300adfbfca19ad
TheKhalifa200 <lovely_khalifa97@hotmail.com>
2022-06-28 09:51:17 +0000

    make it better

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const dialogflowFulfillment = (request, response) => {
 request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     agent.add(console.log(body)) // Print the google web page.
+    agent.add("It works")
   }else{
     agent.add("Did not work")
   }
